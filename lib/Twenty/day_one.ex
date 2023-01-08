e0f7b4afa84a024e065f9b8a7b792d63c7181c77
Elton Campos <eltoncampos36@gmail.com>
2023-01-08 12:10:29 -0300

    :tada: Aoc - Day one part 2 - 2020

--- lib/Twenty/day_one.ex
+++ lib/Twenty/day_one.ex
@@ -202,11 +202,8 @@ defmodule Aoc.Twenty.DayOne do
   1741
   """
 
-  def handle do
-    numbers =
-      @input
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer(&1))
+  def solve_part_one do
+    numbers = parse_input()
 
     Enum.reduce(numbers, 0, fn number, acc ->
       if acc == 0 do
@@ -215,5 +212,22 @@ defmodule Aoc.Twenty.DayOne do
         acc
       end
     end)
+  end
+
+  def solve_part_two do
+    numbers = parse_input()
+    [_head | nubs2] = numbers
+    [_head | nubs3] = nubs2
+
+    for n1 <- numbers, n2 <- nubs2, n3 <- nubs3, n1 + n2 + n3 == 2020 do
+      n1 * n2 * n3
+    end
+    |> hd
+  end
+
+  defp parse_input do
+    @input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer(&1))
   end
 end
