60760d2ffe3e8288a472eccd14f8174c70a8b2ff
Elton Campos <eltoncampos36@gmail.com>
2023-01-10 20:06:32 -0300

    feat: :tada: solve day two part two 2020 and refactor

--- lib/Twenty/day_two.ex
+++ lib/Twenty/day_two.ex
@@ -3,19 +3,51 @@ defmodule Aoc.Twenty.DayTwo do
   @input Inputs.day_two()
 
   def solve_part_one do
+    parse_input()
+    |> Enum.map(&do_solve_part_one/1)
+    |> count()
+  end
+
+  def solve_part_two do
+    parse_input()
+    |> Enum.map(&do_solve_part_two/1)
+    |> count()
+  end
+
+  defp do_solve_part_one([range, letter, pass]) do
+    [start, finish] = parse_range(range)
+    [letter] = String.split(letter, ":", trim: true)
+
+    pass
+    |> get_total_appearances_on_password(letter)
+    |> is_correct_password?(start, finish)
+  end
+
+  defp do_solve_part_two([range, letter, password]) do
+    [start, finish] = parse_range(range)
+
+    [letter] = String.split(letter, ":", trim: true)
+
+    letter_meet_password_correct_position?(password, letter, start, finish)
+  end
+
+  defp letter_meet_password_correct_position?(password, letter, start, finish) do
+    first = String.at(password, start - 1) == letter
+    last = String.at(password, finish - 1) == letter
+
+    case [first, last] do
+      [false, false] -> false
+      [true, true] -> false
+      _ -> true
+    end
+  end
+
+  defp count(result), do: Enum.count(result, fn res -> res == true end)
+
+  defp parse_input() do
     @input
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split(&1, " "))
-    |> Enum.map(fn [range, letter, pass] ->
-      [start, finish] = parse_range(range)
-
-      [letter] = String.split(letter, ":", trim: true)
-
-      pass
-      |> get_total_appearances_on_password(letter)
-      |> is_correct_password?(start, finish)
-    end)
-    |> Enum.count(fn result -> result == true end)
   end
 
   defp parse_range(range) do
